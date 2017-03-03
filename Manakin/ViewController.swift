5c08cfe5e87f95ffe196f244f68cb3d918e1a955
dembis <seriba607@gmail.com>
2017-03-03 15:19:01 +0100

    VeryLongImmutableVariable

--- Manakin/ViewController.swift
+++ Manakin/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     let acceptedByHoundci = "toto"
+    let veryVeryLongVariabeSoLongThatItWontBeAcceptedByHoundCi = "VeryLongImmutableVariable"
 
     override func viewDidLoad() {
         super.viewDidLoad()
